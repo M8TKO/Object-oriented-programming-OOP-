ef7f3ffc5fd1081b0c53441e6d181b37228a0da2
M8TKO <139696938+M8TKO@users.noreply.github.com>
2024-09-08 19:58:01 +0200

    Add files via upload

---
+++ 1. zadaća/1 zad.cpp
@@ -0,0 +1,219 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+/*
+    zelimo u skup staviti sve mjerne jedinice pa 
+    umjesto da pisem rucno sve te rijeci,
+    odlucio sam samo padezne nastavke napisati, 
+    a same rijeci tvorim u funkiciji napuni()
+    gdje na osnovu rijeci stavljam padezne nastavke
+    npr. 
+    milimet + ara = milimetara
+*/
+vector<string> padezi = { "ar","ra","ru","re","rom",
+                        "ri","ara","rima"};
+
+set<string> mjerne_jed;
+
+void napuni(){
+    /*
+        u skup mjerne_jed stavljamo sve oblike rijeci mjernih jedinica
+    */
+    mjerne_jed.insert("mm");
+    for( auto x : padezi )
+        mjerne_jed.insert("milimet" + x );
+
+    mjerne_jed.insert("cm");
+    for( auto x : padezi )
+        mjerne_jed.insert("centimet" + x );
+
+    mjerne_jed.insert("dm");
+    for( auto x : padezi )
+        mjerne_jed.insert("decimet" + x );
+
+}
+
+bool broj(string s){ 
+    /* 
+        provjeravamo je li s broj
+        1. je li sastavljen samo od brojki i tocke
+        2. postoji najvise jedna tocka
+        3. ta tocka nije na pocetku
+        4. ta tocka nije na kraju
+    */
+    return ( s.find_first_not_of(".0123456789") == string::npos &&
+                s.find_first_of(".") == s.find_last_of(".") &&
+                s.find_first_of(".") != 0 &&
+                s.find_last_of(".") != s.length()-1
+                );
+}
+
+int f( string& iznos, string& rijec){
+    /*
+        f-ja koja provjerava je li rijec mjerna jedinica
+        ako jest, u varijabli iznos vracamo promijenjeni iznos 
+        a preko povratne vrijednosti m vracamo koliko znakova
+        moramo obrisati od mjerne jedinice npr.
+        milimetar --> metar | brisemo 4 znaka, mili
+        dm ---> m | brisemo 1 znak
+        ---> iskoristavamo cinjenicu da padezni nastavak ostaje isti
+             te da je rijec metar sastavni dio svih traznih mjernih
+             jedinica :)
+
+        x ce biti odgovarajuca potencija od 10 s kojom dijelimo
+        npr.
+        ako prelazimo iz cm u m onda je x = 100
+    */
+    int m,x;
+    auto it = mjerne_jed.find(rijec);
+
+    /*
+        trazimo je li rijec mjerna jedinica
+    */
+    if( it == mjerne_jed.end() )
+        return 0;
+
+    /*
+        da bismo saznali o kojoj je mjernoj jedinici rijec,
+        dovoljno je gledati je li prvo slovo m, c ili d
+
+    */
+    switch((*it)[0]) {
+        case 'm':
+            m = 4;
+            x = 1000;
+            break;
+        case 'c':
+            m = 5;
+            x = 100;
+            break;
+        case 'd':
+            m = 4;
+            x = 10;
+            break;
+        }
+
+    /*
+        slucaj ako je mjerna jedinica sastavljena od
+        samo 2 slova
+    */
+    if( (*it).length() == 2 )
+        m = 1;
+
+    /*
+        ostatak f-je pretvara u odgovarajuci iznos
+    */
+    double money = stod(iznos) / x;
+    iznos = to_string(money);
+    /*
+        moguce da je iznos == "5.60000"
+        pa ako postoji tocka, micemo sve nule koje su viska
+        i pazimo da broj ne zavrsava s tockom
+    */
+    
+    if( iznos.find_first_of(".") != string::npos ){
+
+        iznos = iznos.substr(0, iznos.find_last_of(".123456789")+1);
+        if( iznos[iznos.length()-1] == '.' )
+            iznos.resize(iznos.length()-1);
+
+    }
+
+    return m;
+} 
+
+
+int main(){
+    
+    string slova;
+    for( int i = 0 ; 'a' + i <= 'z' ; i++)
+        slova += 'a' + i;
+
+    napuni();
+
+    /*
+        ucitavamo linije sve do EOF, kako getline() stane kada
+        dode do '\n' i ne sacuva ga, moramo ga nadodati
+    */
+    string tekst,linija;
+    while( getline(cin,linija) )
+        tekst += linija + '\n';
+
+
+    /*
+        pozicioniramo se na prvu brojku
+    */
+    auto it1 = tekst.find_first_of("0123456789",0);
+    decltype(it1) it2,it3;
+
+    /*  
+        iz teksta odvajamo rijec po rijec sve dok
+        ne dodemo do kraja stringa
+    */
+    while( it1 != string::npos ){
+        
+        /*
+            -   izmedu it1 i it2 stavljamo broj
+            -   it2 ce i npr. "53.242.234......4" shvatiti kao broj
+                medutim, kasnije cemo tu mogucnost odbaciti 
+                kao valjan unos mjerne jedinice
+        */
+        it2 = tekst.find_first_not_of(".0123456789",it1+1);
+
+        if( it2 == string::npos )
+            break;
+
+        string iznos = tekst.substr(it1,it2-it1);
+
+        
+        /*
+            provjeravamo jesmo li izolirali broj
+        */
+        if( broj(iznos) ){
+            
+            int k = iznos.length();
+            /*
+                -   sada moramo vidjeti je li sljedeca rijec 
+                    mjerna jedinica
+                -   it2 pozicioniramo na pocetak sljedece rijeci
+            */
+            it2 = tekst.find_first_not_of(" \n\t\v\f\r",it2);
+            
+            if( it2 == string::npos )   
+                break;
+
+            /*
+                it3 pozicioniramo na kraj sljedece rijeci
+            */
+            it3 = tekst.find_first_not_of(slova,it2);
+
+            /*
+                duljina sljedece rijeci
+            */
+            int duljina = ( it3 == string::npos ) ? tekst.length() : it3-it2;
+            string rijec = tekst.substr(it2,duljina);
+
+            /*
+                gledamo je li ta rijec mjerna jedinica,
+                u funkciji f je opisano kako funkcija radi
+            */
+            int m;
+            if( m = f(iznos,rijec) ){
+                tekst.erase(it2,m);
+                tekst.replace(it1, k , iznos);
+                
+            }
+        }
+        /*
+            idemo na sljedeci broj
+        */
+        it1 = tekst.find_first_of("0123456789",it1+1);
+    }
+
+    cout << tekst;
+    return 0;
+}
---
+++ 1. zadaća/2 zad.cpp
@@ -0,0 +1,169 @@
+#include <iostream>
+#include <list>
+#include <deque>
+#include <cctype>
+
+using namespace std;
+
+bool provjeri(const deque<char> &znakovi ){
+
+    /*
+        trivijalan slucaj :)
+    */
+    if( znakovi.empty() )
+        return true;
+
+    /*
+        pozicioniramo se s pripadnim iteratorima na 
+        kraj i na pocetak
+    */
+    deque<char>::const_iterator i1 = znakovi.cbegin();
+    deque<char>::const_reverse_iterator i2 = znakovi.crbegin();
+    //objasni ovo
+    while( 1 ){
+
+        /*
+            i1 se krece prema desno dok ne naide na slovo
+        */
+        while( i1 != znakovi.cend() && !isalpha(*i1) )
+            i1++;
+
+        /*
+            i2 se krece prema lijevo dok ne naide na slovo
+        */
+        while( i2 != znakovi.crend() && !isalpha(*i2) )
+            i2++;
+
+        /* 
+            ukoliko je doslo do krizanja iteratora, imamo palindrom!
+            ne mozemo usporedivati iterator i obrnuti iterator pa je potrebno napraviti
+            odgovarajucu konverziju pomocu base()
+            ---> ovdje je pokriven i slucaj ako u deque nema slova
+        */
+        if( i1 >= i2.base() - 1 )
+            return true;
+
+        /*
+            usporedujemo pripadna slova i zanemarujemo ako su velika ili malena
+        */
+        if( tolower(*i1) != tolower(*i2) )
+            return false;
+        
+        /*
+            slova su ispala ista i nastavljamo dalje
+        */
+        i1++;
+        i2++;
+    }
+}
+
+
+/*
+    funkcija koja ubacuje znakove iz deque u list<string>
+*/
+void ubaci( const deque<char> &znakovi, list<string> &L ){
+        string s;
+        for( auto x : znakovi ) 
+            s += x;
+        L.push_back(s);
+}
+int main(){
+
+    /*
+        ucitavamo broj i cijeli tekst
+    */
+    int k;
+    cin >> k;
+
+    string tekst,linija;
+    while( getline(cin,linija) )
+        tekst += linija + '\n';
+
+    if( tekst.empty() )
+        return 0;
+
+    /*
+        pomocu i1 se krecemo kroz tekst, a prvo se pozicioniramo
+        na prvo slovo
+    */
+    string::iterator i1 = tekst.begin();
+    deque<char> D;
+    list<string> L;
+
+    while( !isalpha(*i1) )
+            i1++;
+
+    /*
+        u D ubacujemo prvih k slova,
+        dalje cemo prvo slovo izbaciti i ubaciti sljedece,
+        na taj nacin u D uvijek ostaje k slova
+    */
+    /*
+        u varijabli n pamtimo koliko smo slova ubacili
+    */
+    int n = 0;
+    while( i1 != tekst.end() ){
+
+        if( *i1 == '\n')
+            *i1 = ' ';
+
+        if( *i1 == ' ' || isalpha(*i1) ){
+            D.push_back(*i1);
+            if( isalpha(*i1) )
+                n++;
+        }
+
+        if( n == k )
+            break;
+
+        i1++;
+    }
+
+    cout << endl;
+    while( i1 != tekst.end() ){
+
+        if( provjeri(D) )
+            ubaci(D,L);
+
+
+        /*
+            na pocetku je sigurno slovo pa njega maknemo,
+            nadalje, micemo sve znakove s pocetka koji nisu slovo tj. razmake
+        */
+        D.pop_front();
+        while( !isalpha(D.front() ))
+            D.pop_front();
+
+        /*
+            i1 nastavlja dalje dok ne naide na slovo, 
+            razmake isto tako ubacujemo
+        */
+        i1++;
+        while( i1 != tekst.end() && !isalpha(*i1) ){
+
+            if( *i1 == '\n' )
+                *i1 = ' ';
+
+            if( *i1 == ' ' )
+                D.push_back(*i1);
+        
+            i1++;
+        }
+
+        if( i1 == tekst.end() )
+                break;
+
+        /*
+            i1 sada pokazuje na slovo pa je tada izasao iz while petlje,
+            pa to slovo moramo ubaciti
+        */
+        D.push_back(*i1);
+
+
+    }
+
+    for( auto x : L )
+        cout << x << endl;
+
+    return 0;
+}
