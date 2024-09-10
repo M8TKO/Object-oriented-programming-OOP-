44867d26371564130764f48a31e5612de3b14008
M8TKO <139696938+M8TKO@users.noreply.github.com>
2024-09-10 20:51:52 +0200

    Update String Iterators.cpp

--- 1. Palindromes & String Iterators /String Iterators.cpp
+++ 1. Palindromes & String Iterators /String Iterators.cpp
@@ -1,12 +1,12 @@
 /*
-Napišite program koji od korisnika učitava tekst (do EOF). 
-Tekst predstavlja tekst matematičkog zadatka u kojem se javljaju podaci o nekim duljinama iskazanima u milimetrima, centimetrima ili decimetrima 
-(svaki takav podatak koji promatramo sastoji se od jednog pozitivnog broja, bez predznaka i možda s decimalnom točkom, te pripadne mjerne jedinice).
-Pritom se za mjerne jedinice koriste puni nazivi ili oznake mm, cm ili dm. 
-Potrebno je preračunati i prikazati te duljine u metrima (uz pripadni puni naziv ili oznaku m, ovisno o tome je li u unesenom tekstu bio puni naziv ili je bila oznaka - vidi primjere!). 
-Između mjerne jedinice i pripadnog broja može se nalaziti proizvoljan broj praznina (smatramo da su praznine sljedeći znakovi: ' ', '\t', '\n', '\v', '\f', '\r'). 
-Pripazite da razmaci i prelasci u novi red budu očuvani! 
-Pretpostavite da u tekstu koji je korisnik unio nema složenih mjernih jedinica (tako se u ulazu, primjerice, ne može javiti tekst "...ima brzinu 5 dm/h.").
+    Write a program that reads text from the user (until EOF).
+    The text represents a mathematical problem that contains data about some lengths expressed in millimeters, centimeters, or decimeters
+    (each such piece of data consists of a positive number, without a sign, and possibly with a decimal point, followed by the corresponding unit of measurement).
+    Full names or abbreviations mm, cm, or dm are used for the units of measurement.
+    It is necessary to convert and display these lengths in meters (with the corresponding full name or abbreviation m, depending on whether the full name or abbreviation was used in the input - see examples!).
+    Any number of spaces may appear between the unit of measurement and the corresponding number (spaces are considered to be the following characters: ' ', '\t', '\n', '\v', '\f', '\r').
+    Make sure that spaces and line breaks are preserved!
+    Assume that the text entered by the user does not contain compound units of measurement (for example, the input cannot contain text like "...has a speed of 5 dm/h.").
 */
 
 #include <iostream>
@@ -17,12 +17,12 @@ Pretpostavite da u tekstu koji je korisnik unio nema složenih mjernih jedinica 
 using namespace std;
 
 /*
-    zelimo u skup staviti sve mjerne jedinice pa 
-    umjesto da pisem rucno sve te rijeci,
-    odlucio sam samo padezne nastavke napisati, 
-    a same rijeci tvorim u funkiciji napuni()
-    gdje na osnovu rijeci stavljam padezne nastavke
-    npr. 
+    We want to place all measurement units in a set, so
+    instead of manually writing all the words,
+    I decided to write only the case endings,
+    and form the words in the function napuni()
+    where, based on the root word, we add case endings
+    for example,
     milimet + ara = milimetara
 */
 vector<string> padezi = { "ar","ra","ru","re","rom",
@@ -32,7 +32,7 @@ set<string> mjerne_jed;
 
 void napuni(){
     /*
-        u skup mjerne_jed stavljamo sve oblike rijeci mjernih jedinica
+        We add all forms of the measurement units to the set mjerne_jed
     */
     mjerne_jed.insert("mm");
     for( auto x : padezi )
@@ -45,16 +45,15 @@ void napuni(){
     mjerne_jed.insert("dm");
     for( auto x : padezi )
         mjerne_jed.insert("decimet" + x );
-
 }
 
 bool broj(string s){ 
     /* 
-        provjeravamo je li s broj
-        1. je li sastavljen samo od brojki i tocke
-        2. postoji najvise jedna tocka
-        3. ta tocka nije na pocetku
-        4. ta tocka nije na kraju
+        Check if s is a number
+        1. It consists only of digits and a dot
+        2. There is at most one dot
+        3. The dot is not at the beginning
+        4. The dot is not at the end
     */
     return ( s.find_first_not_of(".0123456789") == string::npos &&
                 s.find_first_of(".") == s.find_last_of(".") &&
@@ -65,33 +64,32 @@ bool broj(string s){
 
 int f( string& iznos, string& rijec){
     /*
-        f-ja koja provjerava je li rijec mjerna jedinica
-        ako jest, u varijabli iznos vracamo promijenjeni iznos 
-        a preko povratne vrijednosti m vracamo koliko znakova
-        moramo obrisati od mjerne jedinice npr.
-        milimetar --> metar | brisemo 4 znaka, mili
-        dm ---> m | brisemo 1 znak
-        ---> iskoristavamo cinjenicu da padezni nastavak ostaje isti
-             te da je rijec metar sastavni dio svih traznih mjernih
-             jedinica :)
+        A function that checks if the word is a unit of measurement.
+        If it is, we return the modified value in the variable iznos,
+        and through the return value m we return how many characters
+        must be deleted from the unit of measurement. For example,
+        milimetar --> metar | delete 4 characters, mili
+        dm ---> m | delete 1 character.
+        ---> We use the fact that the case ending remains the same,
+             and that the word metar is part of all the required
+             units of measurement :)
 
-        x ce biti odgovarajuca potencija od 10 s kojom dijelimo
-        npr.
-        ako prelazimo iz cm u m onda je x = 100
+        x will be the corresponding power of 10 we divide by.
+        For example,
+        if we are converting from cm to m, then x = 100.
     */
-    int m,x;
+    int m, x;
     auto it = mjerne_jed.find(rijec);
 
     /*
-        trazimo je li rijec mjerna jedinica
+        We search if the word is a unit of measurement.
     */
     if( it == mjerne_jed.end() )
         return 0;
 
     /*
-        da bismo saznali o kojoj je mjernoj jedinici rijec,
-        dovoljno je gledati je li prvo slovo m, c ili d
-
+        To find out which unit of measurement it is,
+        it's enough to check whether the first letter is m, c, or d.
     */
     switch((*it)[0]) {
         case 'm':
@@ -109,21 +107,21 @@ int f( string& iznos, string& rijec){
         }
 
     /*
-        slucaj ako je mjerna jedinica sastavljena od
-        samo 2 slova
+        In the case where the unit of measurement
+        consists of only 2 letters.
     */
     if( (*it).length() == 2 )
         m = 1;
 
     /*
-        ostatak f-je pretvara u odgovarajuci iznos
+        The rest of the function converts the amount accordingly.
     */
     double money = stod(iznos) / x;
     iznos = to_string(money);
     /*
-        moguce da je iznos == "5.60000"
-        pa ako postoji tocka, micemo sve nule koje su viska
-        i pazimo da broj ne zavrsava s tockom
+        The value might be something like "5.60000",
+        so if there is a dot, we remove any extra zeroes
+        and make sure the number does not end with a dot.
     */
     
     if( iznos.find_first_of(".") != string::npos ){
@@ -137,7 +135,6 @@ int f( string& iznos, string& rijec){
     return m;
 } 
 
-
 int main(){
     
     string slova;
@@ -147,84 +144,83 @@ int main(){
     napuni();
 
     /*
-        ucitavamo linije sve do EOF, kako getline() stane kada
-        dode do '\n' i ne sacuva ga, moramo ga nadodati
+        We read lines until EOF. Since getline() stops when it
+        encounters '\n' and does not save it, we need to add it back.
     */
-    string tekst,linija;
-    while( getline(cin,linija) )
+    string tekst, linija;
+    while( getline(cin, linija) )
         tekst += linija + '\n';
 
-
     /*
-        pozicioniramo se na prvu brojku
+        We position at the first digit.
     */
-    auto it1 = tekst.find_first_of("0123456789",0);
-    decltype(it1) it2,it3;
+    auto it1 = tekst.find_first_of("0123456789", 0);
+    decltype(it1) it2, it3;
 
     /*  
-        iz teksta odvajamo rijec po rijec sve dok
-        ne dodemo do kraja stringa
+        We separate the text word by word until
+        we reach the end of the string.
     */
     while( it1 != string::npos ){
         
         /*
-            -   izmedu it1 i it2 stavljamo broj
-            -   it2 ce i npr. "53.242.234......4" shvatiti kao broj
-                medutim, kasnije cemo tu mogucnost odbaciti 
-                kao valjan unos mjerne jedinice
+            - Between it1 and it2, we place the number.
+            - it2 will even understand something like "53.242.234......4"
+              as a number, but later we will discard that
+              as a valid unit of measurement input.
         */
-        it2 = tekst.find_first_not_of(".0123456789",it1+1);
+        it2 = tekst.find_first_not_of(".0123456789", it1+1);
 
         if( it2 == string::npos )
             break;
 
-        string iznos = tekst.substr(it1,it2-it1);
+        string iznos = tekst.substr(it1, it2-it1);
 
-        
         /*
-            provjeravamo jesmo li izolirali broj
+            We check whether we've isolated a number.
         */
         if( broj(iznos) ){
             
             int k = iznos.length();
             /*
-                -   sada moramo vidjeti je li sljedeca rijec 
-                    mjerna jedinica
-                -   it2 pozicioniramo na pocetak sljedece rijeci
+                - Now we need to check whether the next word
+                  is a unit of measurement.
+                - it2 is positioned at the start of the next word.
             */
-            it2 = tekst.find_first_not_of(" \n\t\v\f\r",it2);
+            it2 = tekst.find_first_not_of(" \n\t\v\f\r", it2);
             
             if( it2 == string::npos )   
                 break;
 
             /*
-                it3 pozicioniramo na kraj sljedece rijeci
+                it3 is positioned at the end of the next word.
             */
-            it3 = tekst.find_first_not_of(slova,it2);
+            it3 = tekst.find_first_not_of(slova, it2);
 
             /*
-                duljina sljedece rijeci
+                Length of the next word.
             */
             int duljina = ( it3 == string::npos ) ? tekst.length() : it3-it2;
-            string rijec = tekst.substr(it2,duljina);
+            string rijec = tekst.substr(it2, duljina);
 
             /*
-                gledamo je li ta rijec mjerna jedinica,
-                u funkciji f je opisano kako funkcija radi
+                We check if that word is a unit of measurement.
+                In function f, it's explained how it works.
             */
             int m;
-            if( m = f(iznos,rijec) ){
-                tekst.erase(it2,m);
-                tekst.replace(it1, k , iznos);
+            if( m = f(iznos, rijec) ){
+                tekst.erase(it2, m);
+                tekst.replace(it1, k, iznos);
                 
             }
         }
         /*
-            idemo na sljedeci broj
+            Move to the next number.
         */
-        it1 = tekst.find_first_of("0123456789",it1+1);
+        it1 = tekst.find_first_of("0123456789", it1+1);
     }
 
     cout << tekst;
     return 0;
 }
+```
